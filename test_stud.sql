2dbd6c6bfc84d9692ad53094a9075518412a96a6
Wavare Sonali <62466919+sona-3099@users.noreply.github.com>
2021-03-16 21:48:08 +0530

    Add files via upload

---
+++ test_stud.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE studentdata
+{
+    Roll_no       integer  PRIMARY KEY,
+    stud_name     varchar(32)      ,
+    address       varchar(64)      ,
+    mobile_no     varchar(16)   ,
+    PAN_no        varchar(20),
+};
+
+insert into studentdata(Roll_no,stud_name,address ,mobile_no, PAN_no )values(1,'saina roy','fc road,pune','2379473630','dsf4653');
+
+insert into studentdata(Roll_no,stud_name,address ,mobile_no, PAN_no )values(2,'Riya Deshpande','Ahmednagar','7359913630','lkfd094653');
+
+insert into studentdata(Roll_no,stud_name,address ,mobile_no, PAN_no )values(3,'Sam Patil','Nashik','9945628630','qpr90653');
+
+insert into studentdata(Roll_no,stud_name,address ,mobile_no, PAN_no )values(4,'Ram Thakur','Ahmednagar','376581530','q270846');
